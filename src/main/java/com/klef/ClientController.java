c55e7324c109dac0d5e48d3526e5ddd5e77ef523
vignesh <vigneshpoluru1277@gmail.com>
2025-10-15 18:45:02 +0530

    Updated

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -1,11 +1,13 @@
 package com.klef;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
-@RequestMapping("/spring")
+@RequestMapping("/spring/")
+@CrossOrigin("*")
 public class ClientController {
 	@GetMapping("/")
 	public String d() {
