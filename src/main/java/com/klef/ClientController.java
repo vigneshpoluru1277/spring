776eac651e465567076a7e0c020c82c64146a665
vignesh <vigneshpoluru1277@gmail.com>
2025-10-15 18:42:44 +0530

    Updated

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -1,10 +1,11 @@
 package com.klef;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
-
+@RequestMapping("/spring")
 public class ClientController {
 	@GetMapping("/")
 	public String d() {
